d364d5c9d5b8e5a798d6af6c3664aa76324571e4
kazurego7 <kazurego7@gmail.com>
2018-10-14 22:31:39 +0900

    Completed/MultiplayerNetwokingTutorial

--- Assets/Script/Health.cs
+++ Assets/Script/Health.cs
@@ -9,21 +9,44 @@ public class Health : NetworkBehaviour {
 
 	[SyncVar (hook = "OnChangeHealth")]
 	public int currentHealth = maxHealth;
+	public bool destoryOnDeath;
 
 	public RectTransform healthBar;
 
+	private NetworkStartPosition[] spawnPoints;
+
+	void Start () {
+		if (isLocalPlayer) {
+			spawnPoints = FindObjectsOfType<NetworkStartPosition> ();
+		}
+	}
+
 	public void TakeDamage (int amount) {
-		if (!isServer)
-			return;
+		if (!isServer) return;
 
 		currentHealth -= amount;
 		if (currentHealth <= 0) {
-			currentHealth = 0;
-			Debug.Log ("Dead!");
+			if (destoryOnDeath) {
+				Destroy (gameObject);
+			} else {
+				currentHealth = maxHealth;
+				RpcRespawn ();
+			}
 		}
 	}
 
 	void OnChangeHealth (int health) {
 		healthBar.sizeDelta = new Vector2 (health, healthBar.sizeDelta.y);
+	}
+
+	[ClientRpc]
+	void RpcRespawn () {
+		if (isLocalPlayer) {
+			Vector3 spawnPoint = Vector3.zero;
+			if (spawnPoints != null && spawnPoints.Length > 0) {
+				spawnPoint = spawnPoints[Random.Range (0, spawnPoints.Length)].transform.position;
+			}
+			transform.position = spawnPoint;
+		}
 	}
 }
